639986723330f0df3e0a8c37764359afcc83e1e1
Husain Imam Shanto <husain.imambd@gmail.com>
2023-08-02 16:11:13 +0200

    Rename in node js

--- Challenge/index.js
+++ Challenge/index.js
@@ -1,8 +1,4 @@
-// to declare file system
+
+
 const fs  = require("fs");
-// create directory
-// fs.mkdirSync("NodeCh");       
-//create and add info to the file
-fs.writeFileSync( "My name is Husain Imam Shanto");
-// append to existing file
-fs.appendFileSync("NodeCh/bio.txt"," I am a Student of Techniocal University of Chemnitz");
+fs.renameSync("NodeCh/bio.txt", "NodeCh/Mybio.txt")
