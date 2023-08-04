04af506d51794221565b3fa30b74e22ec933caa2
Husain Imam Shanto <husain.imambd@gmail.com>
2023-08-04 16:23:56 +0200

    Delete directory

--- Challenge/index.js
+++ Challenge/index.js
@@ -2,20 +2,22 @@
 // challange 2
 const fs = require("fs");
 
-fs.mkdir("shanto", (err)=>{
-    console.log("Folder has been created");
-});
+// fs.mkdir("shanto", (err)=>{
+//     console.log("Folder has been created");
+// });
 
-fs.writeFile("./shanto/bio.txt","My name is Husain Imam Shanto. ", (err)=>{
-    console.log("file has been created");
-});
+// fs.writeFile("./shanto/bio.txt","My name is Husain Imam Shanto. ", (err)=>{
+//     console.log("file has been created");
+// });
 
-fs.appendFile("./shanto/bio.txt","I am a student of TUC", (err)=>{
-    console.log("file has been appended");
-});
+// fs.appendFile("./shanto/bio.txt","I am a student of TUC", (err)=>{
+//     console.log("file has been appended");
+// });
 
-fs.readFile("./shanto/bio.txt","UTF-8", (err,data)=>{
-    console.log("file is being read");
-    console.log(data);
+// fs.readFile("./shanto/bio.txt","UTF-8", (err,data)=>{
+//     console.log("file is being read");
+//     console.log(data);
+// });
+fs.rmdir("./shanto/",(err)=>{
+    console.log("delet");
 });
-
