3ca3f872a6d2d8025f487ad5a9d43d66b0703f37
Husain Imam Shanto <husain.imambd@gmail.com>
2023-08-04 15:33:53 +0200

    Assychronous read file

--- Challenge/index.js
+++ Challenge/index.js
@@ -7,10 +7,16 @@ fs.writeFile("read.txt", "today is an awesome day\n", (err) =>  //call back func
     console.log(err);
 });
 
-fs.appendFile("read.txt", "Lets do coding with full focus", (err) =>  //call back function
+fs.appendFile("read.txt", "Lets do coding with full focus\n", (err) =>  //call back function
 {
-    console.log("file is append");
+    console.log("file is append\n");
     console.log(err);
 });
 // why we need call back function
-//
+
+fs.readFile("read.txt","UTF-8", (err, data) =>  //call back function
+{
+    console.log("file is being read\n");
+    console.log(data);
+    console.log(err);
+});
